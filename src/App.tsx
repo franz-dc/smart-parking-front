ad74b4f87a8c3f2c13011c8b15994df5a7d11f7e
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2022-02-21 18:54:26 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { theme } from './theme';
 import { SnackbarProvider } from 'notistack';
@@ -7,13 +8,21 @@ import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { UserContext } from 'contexts';
 
 // components
 import {
+  // Auth
+  Login,
+  Register,
+  // User
   Home,
   Availability,
   NotFound,
   Reserve,
+  Reservations,
+  AccountSettings,
   // Admin
   AdminDashboard,
   // Dev
@@ -29,38 +38,61 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, (user) => {
+    setUser(user);
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <Helmet
-            titleTemplate='%s - Smart Parking System'
-            defaultTitle='Smart Parking System'
-          />
-          <SnackbarProvider>
-            <LocalizationProvider dateAdapter={DateAdapter}>
-              <BrowserRouter>
-                <Routes>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/availability' element={<Availability />} />
-                  <Route path='/reserve' element={<Reserve />} />
+      <UserContext.Provider value={user}>
+        <CssBaseline />
+        <HelmetProvider>
+          <QueryClientProvider client={queryClient}>
+            <Helmet
+              titleTemplate='%s - Smart Parking System'
+              defaultTitle='Smart Parking System'
+            />
+            <SnackbarProvider>
+              <LocalizationProvider dateAdapter={DateAdapter}>
+                <BrowserRouter>
+                  <Routes>
+                    {/* Auth */}
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
 
-                  {/* Admin */}
-                  <Route path='/admin/dashboard' element={<AdminDashboard />} />
+                    {/* User */}
+                    <Route path='/' element={<Home />} />
+                    <Route path='/availability' element={<Availability />} />
+                    <Route path='/reserve' element={<Reserve />} />
+                    <Route path='/reservations' element={<Reservations />} />
+                    <Route
+                      path='/account-settings'
+                      element={<AccountSettings />}
+                    />
 
-                  {/* Dev */}
-                  <Route path='/dev/seed' element={<Seed />} />
+                    {/* Admin */}
+                    <Route
+                      path='/admin/dashboard'
+                      element={<AdminDashboard />}
+                    />
 
-                  {/* Misc */}
-                  <Route path='*' element={<NotFound />} />
-                </Routes>
-              </BrowserRouter>
-            </LocalizationProvider>
-          </SnackbarProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </HelmetProvider>
+                    {/* Dev */}
+                    <Route path='/dev/seed' element={<Seed />} />
+
+                    {/* Misc */}
+                    <Route path='*' element={<NotFound />} />
+                  </Routes>
+                </BrowserRouter>
+              </LocalizationProvider>
+            </SnackbarProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </HelmetProvider>
+      </UserContext.Provider>
     </ThemeProvider>
   );
 };
