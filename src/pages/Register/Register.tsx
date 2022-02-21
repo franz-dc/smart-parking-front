f607edae123404407acce40090c6d35c3b20518c
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2022-02-21 18:54:07 +0800

    Add register page

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,100 @@
+import { useNavigate } from 'react-router-dom';
+import { AuthBase } from 'components';
+import { LoadingButton } from '@mui/lab';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-mui';
+import * as Yup from 'yup';
+import { authService } from 'services';
+import { useSnackbar } from 'notistack';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const validationSchema = Yup.object()
+    .shape({
+      email: Yup.string().email('Invalid email').required('Required'),
+      password: Yup.string()
+        .min(8, 'Password must be at least 8 characters')
+        .required('Required'),
+      confirmPassword: Yup.string()
+        .required('Required')
+        .test('passwordsMatch', 'Passwords must match', function (value) {
+          return this.parent.password === value;
+        }),
+    })
+    .required();
+
+  const handleSubmit = async ({
+    confirmPassword,
+    ...userCredentials
+  }: Yup.InferType<typeof validationSchema>) => {
+    try {
+      const user = await authService.register(userCredentials);
+
+      console.log(user);
+      navigate('/account-settings');
+    } catch (err: any) {
+      let message = 'Something went wrong';
+
+      switch (err?.code) {
+        case 'auth/email-already-in-use':
+          message = 'Email already exists';
+          break;
+        case 'auth/invalid-email':
+          message = 'Invalid email';
+          break;
+        default:
+          console.error(err?.code || message);
+          break;
+      }
+
+      enqueueSnackbar(message, { variant: 'error' });
+    }
+  };
+
+  return (
+    <AuthBase type='register'>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+          confirmPassword: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field
+              component={TextField}
+              id='email'
+              name='email'
+              label='Email'
+            />
+            <Field
+              component={TextField}
+              id='password'
+              name='password'
+              label='Password'
+              type='password'
+            />
+            <Field
+              component={TextField}
+              id='confirmPassword'
+              name='confirmPassword'
+              label='Confirm Password'
+              type='password'
+            />
+            <LoadingButton type='submit' fullWidth loading={isSubmitting}>
+              Register
+            </LoadingButton>
+          </Form>
+        )}
+      </Formik>
+    </AuthBase>
+  );
+};
+
+export default Login;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,14 @@
+// Auth
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+
+// User
 export { default as Home } from './Home';
 export { default as Availability } from './Availability';
 export { default as Reserve } from './Reserve';
+export { default as Reservations } from './Reservations';
 export { default as NotFound } from './NotFound';
+export { default as AccountSettings } from './AccountSettings';
 
 // Admin
 export { default as AdminDashboard } from './AdminDashboard';
