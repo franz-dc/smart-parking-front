6aadea6b2a779869515dc7d18ed9354634bdb1f3
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2022-02-25 03:41:34 +0800

    Refactor getUsersByIds

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -34,9 +34,30 @@ export const usersService = {
     return data.data() as IUser;
   },
   getUsersByIds: async (userIds: string[]): Promise<IUser[]> => {
-    const q = query(usersRef, where(documentId(), 'in', userIds));
-    const data = await getDocs(q);
-    return mapData(data);
+    const uniqueUserIds = [...new Set(userIds)];
+
+    let queries = [];
+
+    for (let i = 0; i < uniqueUserIds.length; i += 10) {
+      queries.push(
+        query(
+          collection(db, 'users'),
+          where(documentId(), 'in', uniqueUserIds.slice(i, i + 10))
+        )
+      );
+    }
+
+    let userDocsSnaps = [];
+
+    for (let i = 0; i < queries.length; i++) {
+      userDocsSnaps.push(getDocs(queries[i]));
+    }
+
+    userDocsSnaps = await Promise.all(userDocsSnaps);
+
+    const userDocs = userDocsSnaps.map((snap) => mapData(snap)).flat();
+
+    return userDocs;
   },
   updateUser: async (user: any) => {
     const auth = getAuth();
