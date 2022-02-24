526dc6aea42340fbc79ae9631b8957275a6607e0
Franz Joshua Dela Cruz <franzdelacruz.stack@gmail.com>
2022-02-24 20:36:57 +0800

    Add getLotByDetails to lotsService

--- src/services/lotsService.ts
+++ src/services/lotsService.ts
@@ -1,6 +1,12 @@
 import { db } from 'firebase-config';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { ILot } from 'types';
+
+interface GetLotByDetailsParams {
+  floor: string;
+  area: string;
+  lotNumber: number;
+}
 
 const lotsRef = collection(db, 'lots');
 
@@ -11,5 +17,25 @@ export const lotsService = {
       id: doc.id,
       ...(doc.data() as Omit<ILot, 'id'>),
     }));
+  },
+  getLotByDetails: async ({
+    floor,
+    area,
+    lotNumber,
+  }: GetLotByDetailsParams): Promise<ILot> => {
+    const q = query(
+      lotsRef,
+      where('floor', '==', floor),
+      where('area', '==', area),
+      where('lotNumber', '==', lotNumber)
+    );
+    const data = await getDocs(q);
+
+    if (data.docs.length === 0) throw new Error('Lot not found');
+
+    return {
+      id: data.docs[0].data().id,
+      ...(data.docs[0].data() as Omit<ILot, 'id'>),
+    };
   },
 };
